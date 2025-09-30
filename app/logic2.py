74b065d8caf5e74483f85e567cb2e98eff2a7fc4
gterry001 <72092025+gterry001@users.noreply.github.com>
2025-09-30 21:41:22 +0200

    Update logic2.py

--- app/logic2.py
+++ app/logic2.py
@@ -1,1 +1,24 @@
 import pandas as pd
+import os
+
+BASE_DIR = Path(__file__).resolve().parent
+print(BASE_DIR)
+print("Contenido de /var/task/app:", os.listdir(BASE_DIR))
+
+
+def run_analysis():
+    portfolio = pd.read_excel(portfolio_path)
+    return {
+        "ordenes":"leido"
+    }
+
+
+
+
+
+
+
+
+
+
+
